a209c835e1fe9d098fb7aa819c1a579d924b21c3
Yefim <efim.yakunin@gmail.com>
2023-02-16 13:47:43 +0200

    Main Storyboard with different color

--- NetworkingMentoringSession/Episode/EpisodeComposer.swift
+++ NetworkingMentoringSession/Episode/EpisodeComposer.swift
@@ -4,7 +4,9 @@ final class EpisodeComposer {
     
   static func build (podcastId: String) -> UIViewController {
     let presenter = EpisodePresenter(podcastId: podcastId)
-    let controller = EpisodeViewController()
+      let storyboard = UIStoryboard(name: "Main", bundle: nil)
+      let storyboardID = String(describing: EpisodeViewController.self)
+      let controller = storyboard.instantiateViewController(withIdentifier: storyboardID) as! EpisodeViewController
     controller.presenter = presenter
     presenter.view = controller
     return controller
--- NetworkingMentoringSession/Podcast/PodcastComposer.swift
+++ NetworkingMentoringSession/Podcast/PodcastComposer.swift
@@ -4,7 +4,9 @@ final class PodcastComposer {
     
     static func build (genreId: Int) -> UIViewController {
         let presenter = PodcastPresenter(genreId: genreId)
-        let controller = PodcastViewController()
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        let storyboardID = String(describing: PodcastViewController.self)
+        let controller = storyboard.instantiateViewController(withIdentifier: storyboardID) as! PodcastViewController
         controller.presenter = presenter
         presenter.view = controller
         return controller
